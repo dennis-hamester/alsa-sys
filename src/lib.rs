e3fcfc14b3709ca059a8c65d045cc9ccc211d5ad
Dennis Hamester <dennis.hamester@gmail.com>
2022-06-23 15:30:55 +0200

    Silence Clippy warnings caused by the generated bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
+#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::redundant_static_lifetimes)]
+#![allow(clippy::useless_transmute)]
 
 use libc::{pid_t, pollfd, timespec, timeval, FILE};
 
